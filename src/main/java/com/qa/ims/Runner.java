8209b0053a1b9c6802cbf1235a386e8e3782fdb7
David Cuero <W5845542@student.hccs.edu>
2021-06-01 17:17:29 -0500

    Update Runner.java

--- src/main/java/com/qa/ims/Runner.java
+++ src/main/java/com/qa/ims/Runner.java
@@ -1,16 +1,75 @@
 package com.qa.ims;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.Scanner;
 
 public class Runner {
-
-	public static final Logger LOGGER = LogManager.getLogger();
+	
+	static void menuMessage() {
+		System.out.println("Welcome to the Inventory Managment System!");
+		System.out.println();
+		System.out.println("Please Choose an option below to continue: ");
+		System.out.println();
+		System.out.println("A: Add New Customer.");
+		System.out.println();
+		System.out.println("B: Show All Customers.");
+		System.out.println();
+		System.out.println("C: Show All Items.");
+		System.out.println();
+		System.out.println("D: Create New Order.");
+		System.out.println();
+		System.out.println("E: Add Item to Order.");
+		System.out.println();
+		System.out.println("F: Show All Orders.");
+		System.out.println();
+		System.out.println("G: Exit.");
+		
+	}
+	
+	static void initiatePortal() {
+		char option = '\0';
+		menuMessage();
+		
+		do {
+			SqlCon myCon = new SqlCon();
+			Scanner myInput = new Scanner(System.in);
+			char chosenInput = myInput.next().charAt(0);
+			option = Character.toUpperCase(chosenInput);
+			switch(option) {
+			case 'A':
+				myCon.addCustomer();
+				menuMessage();
+				break;
+			case 'B':
+				myCon.showAllCustomers();
+				menuMessage();
+				break;
+			case 'C':
+				myCon.showItems();
+				menuMessage();
+				break;
+			case 'D':
+				myCon.createOrder();
+				menuMessage();
+				break;
+			case 'E':
+				myCon.addItemsToOrder();
+				menuMessage();
+				break;
+			case 'F':
+				myCon.showOrders();
+				menuMessage();
+				break;
+			default:
+				System.out.println("Please Choose a valid option: A, B, C, D, E, or F.");
+				menuMessage();
+				break;
+			}
+		} while(option != 'G');
+		System.out.println("Thank you for using the inventory managment system!");
+	}
 
 	public static void main(String[] args) {
-		IMS ims = new IMS();
-		ims.imsSystem();
-		LOGGER.info("SO LONG!");
+		// TODO Auto-generated method stub
+		initiatePortal();
 	}
 
 }
